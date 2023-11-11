ffbb8f109fc248321c275e3bca810fcc2528cdfa
Janik Krause <jkrause@leipziger-messe.local>
2023-11-11 12:01:31 +0100

    Added sql-insert-template for CPUs

---
+++ data-staging/insertCPUComponent.sql
@@ -0,0 +1,18 @@
+insert into Components
+set ComponentTypeID = (
+    select ComponentTypeID
+    from ComponentTypes
+    where TypeName = 'CPU'
+),
+Name = 'Ryzen 5 5600X',
+ManufacturerID = (
+    select ManufacturerID 
+    from Manufacturers
+    where ManufacturerName = 'AMD'
+),
+Price = 148.68,
+CoreCount = 6,
+ClockSpeed = '3.7',
+BoostClockSpeed = '4.6', 
+TDP = 65,
+SMT = 'TRUE';
